6f249603e819708508b9afdee49e0b6a4439a5c4
lauratorralba <64459116+lauratorralba@users.noreply.github.com>
2020-04-30 23:26:45 -0400

    Update code.c
    u

--- code.c
+++ code.c
@@ -3,6 +3,17 @@
 int main() {
    /* my first program in C */
    printf("Hello, World! \n");
+   int i=0;
+   int flag=0;
+   for(i=0;i<2;i++){
+      printf("this is a new action\n");
+      if(i==1){
+         flag =1 ;
+      }
+   }
+   if(flag==1){
+      printf("finished correctly");
+   }
 
    return 0;
 }
